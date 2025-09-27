64025785581ac4ca56dbed229486119219bc1903
Noel <noelmugisha332@gmail.com>
2025-09-28 00:02:49 +0200

    Added BillingGrpcService to handle patient billing

---
+++ Billing-Service/src/main/java/com/demo/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,28 @@
+package com.demo.billingservice.grpc;
+
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase{
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(
+            billing.BillingRequest billingRequest,
+            StreamObserver <BillingResponse> responseObserver
+    ) {
+        log.info("Billing request received {}", billingRequest.toString());
+        // Business Logic goes here
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
