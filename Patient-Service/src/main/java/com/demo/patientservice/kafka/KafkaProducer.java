f59286419b7e61a0feefb03766b434c20e0dbd29
Noel <noelmugisha332@gmail.com>
2025-10-11 20:23:01 +0200

    Added the PATIENT CREATED event message

--- Patient-Service/src/main/java/com/demo/patientservice/kafka/KafkaProducer.java
+++ Patient-Service/src/main/java/com/demo/patientservice/kafka/KafkaProducer.java
@@ -1,13 +1,30 @@
 package com.demo.patientservice.kafka;
 
+import com.demo.patientservice.entity.Patient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
 @Service
+@Slf4j
 public class KafkaProducer {
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(Patient patient) {
+        var event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED").build();
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error sending PATIENT_CREATED event {}", event);
+        }
     }
 }
